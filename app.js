aae669fc7cba36f0c51db16ce682bb1654588ab7
mic <micmor.m@gmail.com>
2020-11-17 23:06:52 -0500

    add getPlayerChoice

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const ROCK = "Rock";
+const PAPER = "Paper";
+const SCISSOR = "Scissor";
+const DEFAULT_CHOICE = ROCK;
+const gameIsRunning = false;
+
+const startGameBtn = document.getElementById("start-game-btn");
+
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSOR}`, "").toUpperCase();
+
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSOR) {
+    alert("Invalid choic. We chose Rock for you!");
+    return DEFAULT_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game isstarting...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
