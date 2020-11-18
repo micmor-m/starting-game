819e8c2f80a0ef65c8cf570e2e8e4bd8850fd556
mic <micmor.m@gmail.com>
2020-11-17 23:38:24 -0500

    add getComputerChoice and getWinner

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
-const ROCK = "Rock";
-const PAPER = "Paper";
-const SCISSOR = "Scissor";
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSOR = "SCISSOR";
 const DEFAULT_CHOICE = ROCK;
-const gameIsRunning = false;
+const RESEULT_DRAW = "DRAW";
+const RESULT_PLAYER_WIN = "PLAYER WINS";
+const RESULT_COMPUTER_WIN = "COMPUTER WINS";
+
+let gameIsRunning = false;
 
 const startGameBtn = document.getElementById("start-game-btn");
 
 const getPlayerChoice = function () {
   const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSOR}`, "").toUpperCase();
-
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSOR) {
-    alert("Invalid choic. We chose Rock for you!");
+    alert("Invalid choice. We chose Rock for you!");
     return DEFAULT_CHOICE;
   }
   return selection;
+};
+
+const getComputerChoice = function () {
+  const randomChoice = Math.random();
+  if (randomChoice <= 0.33) {
+    return ROCK;
+  } else if (randomChoice <= 0.66) {
+    return PAPER;
+  } else {
+    return SCISSOR;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESEULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSOR) ||
+    (cChoice === SCISSOR && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WIN;
+  } else {
+    return RESULT_COMPUTER_WIN;
+  }
 };
 
 startGameBtn.addEventListener("click", function () {
@@ -21,7 +49,9 @@ startGameBtn.addEventListener("click", function () {
     return;
   }
   gameIsRunning = true;
-  console.log("Game isstarting...");
+  console.log("Game is starting...");
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const computerSelection = getComputerChoice();
+  const winner = getWinner(computerSelection, playerSelection);
+  console.log(winner);
 });
